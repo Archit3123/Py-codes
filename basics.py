4c39c93568435585c069ee2ff249e5042b9c37c0
Archit Gautam <architechcu07@gmail.com>
2026-05-30 09:02:20 +0530

    first commit

---
+++ basics.py
@@ -0,0 +1,18 @@
+# Day 1 of learning Print statements, Comments, Escape Characters & Sequences in Python
+# single line comment 
+
+'''We,ll 
+print ("Hello ", my name is the "Archit Gautam" )
+and understand the concept of escape characters and sequences 
+and also sep, end parameters in print statements'''
+
+# sep is used to separate the arguments with a custom separator. 
+# end is used to specify what to print at the end of the statement instead of the default newline character.
+
+# multi line comments
+
+print("hello world")
+
+print("\"Hello \", my name is the \"Archit Gautam\" ", "and its day 1", 7, sep="|", end="!!\n")
+print("\n")
+print("ready")
